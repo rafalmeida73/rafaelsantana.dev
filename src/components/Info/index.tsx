4288236d98224e2198d7acdbc05add3bb1e10d57
rafael <rafael.santana@webjump.com.br>
2023-03-04 10:40:41 -0300

    infos content

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+
+import styles from './Info.module.scss';
+import { InfoProps } from './types';
+
+export const Info: FC<InfoProps> = ({
+  title,
+  description,
+  time,
+  full = false,
+}) => {
+  return (
+    <div
+      className={
+        full
+          ? `${styles.container} col m12 l12`
+          : `${styles.container} col m6 l6`
+      }
+    >
+      {!title?.link ? (
+        <p className={styles.title}>{title?.text}</p>
+      ) : (
+        <a
+          href={title?.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={title?.ariaLabel}
+          className={styles.title}
+        >
+          {title?.text}
+        </a>
+      )}
+      <p>{time}</p>
+      {!description?.link ? (
+        <p>{description?.text}</p>
+      ) : (
+        <a
+          href={description?.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={description?.ariaLabel}
+        >
+          {description?.text}
+        </a>
+      )}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { GithubIcon } from '@/components/GithubIcon';
 import { ImageProfile } from '@/components/ImageProfile';
+import { Info } from '@/components/Info';
 import { LanguageIcon } from '@/components/LanguageIcon';
 import { LinkedinIcon } from '@/components/LinkedinIcon';
 import { WhatsAppIcon } from '@/components/WhatsAppIcon';
@@ -14,7 +15,7 @@ export default function Home() {
   const t = useTranslations('Home');
 
   const handleChangeColor = useCallback((color: string) => {
-    document.documentElement.style.setProperty('--primary', 'red');
+    document.documentElement.style.setProperty('--primary', color);
   }, []);
 
   return (
@@ -77,18 +78,188 @@ export default function Home() {
       </main>
 
       <div className="container">
-        <section>
-          <div className={styles.me}>
-            <h2>{t('aboutMe')}</h2>
-            <p>{t('aboutMeDescription')}</p>
+        <section className={styles.me}>
+          <h2>{t('aboutMe')}</h2>
+          <p>{t('aboutMeDescription')}</p>
+        </section>
+
+        <section className={`${styles.infos} col s12 m12 l6`}>
+          <h3>FORMAÇÃO ACADÊMICA</h3>
+          <div className="row">
+            <Info
+              title={{
+                text: 'Análise e Desenvolvimento de Sistemas',
+              }}
+              time="2019 - 2021"
+              description={{ text: 'Uninove' }}
+            />
+            <Info
+              title={{
+                text: 'Full-stack Developer',
+              }}
+              time="2022 - 2023"
+              description={{
+                text: 'Senac',
+              }}
+            />
+          </div>
+
+          <h3>EXPERIÊNCIA</h3>
+          <div className="row">
+            <Info
+              title={{
+                text: 'Desenvolvedor Full Stack',
+              }}
+              time="Fev/2021 - Set/2021"
+              description={{
+                text: 'Valiant Group',
+                ariaLabel: 'Valiant Group',
+                link: 'https://valiantgroup.com.br/',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Desenvolvedor Mobile',
+              }}
+              time="Set/2021 - Atual"
+              description={{
+                text: 'Web Jump',
+                ariaLabel: 'Web Jump',
+                link: 'https://webjump.com.br/',
+              }}
+            />
+          </div>
+
+          <h3>CURSOS COMPLEMENTARES</h3>
+          <div className="row">
+            <Info
+              title={{
+                text: 'Web Full Stack Node',
+                link: 'https://drive.google.com/file/d/1i-797GuLtIlLSNEbPE6gFIU9RRjS1JIv/view?usp=sharing',
+                ariaLabel: 'Certificado Web Full Stack Node',
+              }}
+              time="185 horas"
+              description={{
+                text: 'Digital House',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Inglês',
+              }}
+              time="2016-2018"
+              description={{
+                text: 'Cidadão Pró-Mundo',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Microsoft Excel',
+                link: 'https://www.udemy.com/certificate/UC-J026Q6DE/',
+                ariaLabel: 'Certificado Microsoft Excel',
+              }}
+              time="11 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'HTML e CSS na prática',
+                link: 'https://drive.google.com/file/d/1b9UyZWeGBx43Gm_q0H6wvBbE9eLNOjwl/view?usp=sharing',
+                ariaLabel: 'Certificado HTML e CSS na prática',
+              }}
+              time="24 horas"
+              description={{
+                text: 'Fundação Bradesco',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Web Moderno com JavaScript',
+                link: 'https://www.udemy.com/certificate/UC-N4JLM3OE/',
+                ariaLabel: 'Certificado Web Moderno com JavaScript',
+              }}
+              time="76 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Banco de dados SQL',
+                link: 'https://www.udemy.com/certificate/UC-6RK781ZN/',
+                ariaLabel: 'Certificado Banco de dados SQL',
+              }}
+              time="36 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'React Js do zero ao avançado na pratica',
+                link: 'https://www.udemy.com/certificate/UC-0YQZQZ7Y/',
+                ariaLabel:
+                  'Certificado React Js do zero ao avançado na pratica',
+              }}
+              time="18 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Aplicativos mobile do zero com React Native e Redux',
+                link: 'https://www.udemy.com/certificate/UC-fec333ae-cced-400f-ad23-e734934d4f71/',
+                ariaLabel:
+                  'Certificado Aplicativos mobile do zero com React Native e Redux',
+              }}
+              time="31 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Aprenda NextJS, GraphQL e Leaflet na prática!',
+                link: 'https://www.udemy.com/certificate/UC-408ca299-98c6-4653-a05e-8d4a6fb61b21/',
+                ariaLabel:
+                  'Certificado Aprenda NextJS, GraphQL e Leaflet na prática!',
+              }}
+              time="9 horas"
+              description={{
+                text: 'Udemy',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Trilha React Native do programa Ignite',
+                link: 'https://app.rocketseat.com.br/api/certificates/pdf/fece1ed0-e703-4253-9b39-b667120b0a32',
+                ariaLabel: 'Certificado Trilha React Native do programa Ignite',
+              }}
+              time="100 horas"
+              description={{
+                text: 'Rocketseat',
+              }}
+            />
+            <Info
+              title={{
+                text: 'Trilha ReactJS do programa Ignite',
+                link: 'https://app.rocketseat.com.br/api/certificates/pdf/14a5304c-b6bb-43aa-9d97-db267aa8c405',
+                ariaLabel: 'Certificado Trilha ReactJS do programa Ignite',
+              }}
+              time="100 horas"
+              description={{
+                text: 'Rocketseat',
+              }}
+              full
+            />
           </div>
         </section>
 
         <section>
-          <div className="col s12 m12 l6">
-            <h3>FORMAÇÃO ACADÊMICA</h3>
-            <div className="row" />
-          </div>
+          <h3>PROJETOS</h3>
         </section>
       </div>
     </>
