22917a967fff6ca00eeef3255189d923e44db373
Rafael Santana <rafaelsantana7213@gmail.com>
2023-03-10 10:44:52 -0300

    fix: carrousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,17 +8,19 @@ import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    const carousel = document.querySelectorAll('.carousel');
-    const tooltip = document.querySelectorAll('.tooltipped');
-    const changeColor = document?.getElementById?.('color');
-    if (carousel && (window as any)?.M) {
-      (window as any).M.Carousel.init(carousel);
-    }
-    if ((window as any)?.M) {
-      (window as any).M.Tooltip.init(tooltip);
-    }
+    if (window !== undefined) {
+      const carousel = document.querySelectorAll('.carousel');
+      const tooltip = document.querySelectorAll('.tooltipped');
+      const changeColor = document?.getElementById?.('color');
+      if (carousel) {
+        (window as any).M.Carousel.init(carousel);
+      }
+      if ((window as any)?.M) {
+        (window as any).M.Tooltip.init(tooltip);
+      }
 
-    if (changeColor) changeColor.style.display = 'flex';
+      if (changeColor) changeColor.style.display = 'flex';
+    }
   }, []);
 
   return (
