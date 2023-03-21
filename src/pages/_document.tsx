90645636f5263caf42646a887ca2da8e81da0631
Rafael Santana <rafaelsantana7213@gmail.com>
2023-03-21 16:52:30 -0300

    chore: update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
         {/* Import materialize.css */}
         <link
           rel="stylesheet"
