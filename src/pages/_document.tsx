c75e73fc39490b7df0aef4a4a12afb6d7ab82dc4
Rafael Santana <rafaelsantana7213@gmail.com>
2023-03-24 10:05:23 -0300

    chore: update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,9 +21,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#363636" />
         {/* Import materialize.css */}
         <link
           rel="stylesheet"
