0e02dd31dc2a7f9cf05ccd44e608b2e20a709cec
Rafael Santana <rafael.almeida@medsystems.com.br>
2023-11-08 22:14:42 -0300

    chore: remove ;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,7 +100,6 @@ export default async function RootLayout({
          })(window, document, "clarity", "script", "${clarityId}");`,
           }}
         />
-        ;
       </body>
     </html>
   );
