318741b9e402ffc26f2bba64fc8901ec532ba28c
Rafael Santana <rafaelsantana7213@gmail.com>
2023-12-13 22:19:07 -0300

    fix: remove pwa

--- next.config.js
+++ next.config.js
@@ -5,15 +5,8 @@ const withNextIntl = require('next-intl/plugin')(
   './src/i18n.ts'
 );
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
-
-module.exports = withPWA(
-  withNextIntl({
+module.exports = withNextIntl({
   reactStrictMode: true,
   swcMinify: true
-  })
-)
+})
 
