928a871357724da5bd184eed28387973647a3c69
Rafael Santana <rafaelsantana7213@gmail.com>
2024-06-23 23:11:46 -0300

    feat: change favicon color

--- src/components/ChangeColor/index.tsx
+++ src/components/ChangeColor/index.tsx
@@ -5,6 +5,8 @@ import { useCallback, useState, useEffect } from 'react';
 import { useColor } from '@/hooks/useColor';
 import { useTranslations } from 'next-intl';
 
+import { svgIcon } from './svgIcon';
+
 export const ChangeColor = () => {
   const [loading, setLoading] = useState(true);
 
@@ -12,10 +14,29 @@ export const ChangeColor = () => {
 
   const t = useTranslations('Home');
 
+  // Function to update the favicon with the given SVG
+  function changeFaviconWithIcon(svgData: string) {
+    // Convert SVG to base64-encoded data URL
+    const svgBase64 = btoa(svgData);
+    const faviconURL = `data:image/svg+xml;base64,${svgBase64}`;
+
+    let link = document.querySelector("link[rel*='icon']") as HTMLLinkElement;
+
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+    // Update the favicon
+    link.href = faviconURL;
+  }
+
   const handleChangeColor = useCallback(
     (colorValue: string) => {
       try {
         setColor(colorValue);
+        changeFaviconWithIcon(svgIcon(colorValue));
         document.documentElement.style.setProperty('--primary', colorValue);
       } catch (error) {
         // eslint-disable-next-line no-console
