c1707ab52ea916140fba7c668f38cf088650c13b
Rafael Santana <rafaelsantana7213@gmail.com>
2024-08-15 17:07:35 -0300

    refactor: create objects to projects, techs and formations

---
+++ src/utils/projects.ts
@@ -0,0 +1,362 @@
+export const projects = [
+  {
+    hasMockup: false,
+    title: 'Broto',
+    description: 'projects.broto.description',
+    app: true,
+    link: '',
+    android: 'https://play.google.com/store/apps/details?id=broto.mobile.app',
+    ios: 'https://apps.apple.com/br/app/broto/id1619769755',
+    images: [
+      {
+        image: '/img/broto/broto.webp',
+        text: 'projects.broto.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: [
+      'React Native',
+      'Styled Components',
+      'Firebase',
+      'Magento',
+      'Graphql',
+    ],
+  },
+  {
+    hasMockup: false,
+    title: 'Natural da terra',
+    description: 'projects.nt.description',
+    app: true,
+    link: '',
+    android:
+      'https://play.google.com/store/apps/details?id=br.com.naturaldaterra.app',
+    ios: 'https://apps.apple.com/br/app/natural-da-terra-novo/id1583935893',
+    images: [
+      {
+        image: '/img/naturalTerra/nt.webp',
+        text: 'projects.nt.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: [
+      'React Native',
+      'Styled Components',
+      'Firebase',
+      'Magento',
+      'Graphql',
+    ],
+  },
+  {
+    hasMockup: false,
+    title: 'Hortifruti',
+    description: 'projects.ht.description',
+    app: true,
+    link: '',
+    android:
+      'https://play.google.com/store/apps/details?id=br.com.hortifruti.app',
+    ios: 'https://apps.apple.com/br/app/hortifruti-novo/id1583936154',
+    images: [
+      {
+        image: '/img/hortifruti/ht.webp',
+        text: 'projects.ht.imageAlt',
+      },
+    ],
+    techs: [
+      'React Native',
+      'Styled Components',
+      'Firebase',
+      'Magento',
+      'Graphql',
+    ],
+  },
+  {
+    hasMockup: false,
+    title: 'MedSales',
+    description: 'projects.medSales.description',
+    app: true,
+    link: '',
+    android:
+      'https://play.google.com/store/apps/details?id=com.medsystems.propostasvendas',
+    ios: 'https://apps.apple.com/br/app/medsales/id6471970812',
+    images: [
+      {
+        image: '/img/medSales/medSales.webp',
+        text: 'projects.medSales.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: ['React Native', 'Styled Components'],
+  },
+  {
+    hasMockup: false,
+    title: 'MedInventory',
+    description: 'projects.medInventory.description',
+    app: true,
+    link: '',
+    android:
+      'https://play.google.com/store/apps/details?id=br.com.helpdesk.medsystems.medinventory',
+    ios: 'https://apps.apple.com/br/app/medinventory/id6474357759',
+    images: [
+      {
+        image: '/img/medInventory/medInventory.webp',
+        text: 'projects.medInventory.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: ['React Native', 'Styled Components'],
+  },
+  {
+    hasMockup: false,
+    title: 'MedEntregas',
+    description: 'projects.medEntregas.description',
+    app: true,
+    link: '',
+    android:
+      'https://play.google.com/store/apps/details?id=br.com.helpdesk.medsystems.app',
+    ios: 'https://apps.apple.com/br/app/medentregas/id6471966535',
+    images: [
+      {
+        image: '/img/medEntregas/medEntregas.webp',
+        text: 'projects.medEntregas.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: ['React Native', 'Styled Components'],
+  },
+  {
+    hasMockup: false,
+    title: 'MedStock',
+    description: 'projects.medStock.description',
+    app: false,
+    link: 'https://medstock.medsystems.com.br/',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/medStock/medStock.webp',
+        text: 'projects.medStock.imageAlt',
+        mockup: false,
+      },
+    ],
+    techs: [
+      'Next.js',
+      'MUI',
+      'Firebase',
+      'React Query',
+      'TypeScript',
+      'React Hook Form',
+    ],
+  },
+  {
+    hasMockup: true,
+    title: 'Pokemon',
+    description: 'projects.pokemon.description',
+    app: false,
+    link: '',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/pokemon/pokemon1.webp',
+        text: 'projects.splashScreen',
+        mockup: true,
+      },
+      {
+        image: '/img/pokemon/pokemon2.webp',
+        text: 'projects.pokemon.infoPage',
+        mockup: true,
+      },
+      {
+        image: '/img/pokemon/pokemon3.webp',
+        text: 'projects.initialPage',
+        mockup: true,
+      },
+    ],
+    techs: ['React Native', 'Styled Components', 'Firebase'],
+  },
+  {
+    hasMockup: true,
+    title: 'Todo List',
+    description: 'projects.todoList.description',
+    app: false,
+    link: '',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/todo/todo1.webp',
+        text: 'projects.initialPage',
+        mockup: true,
+      },
+      {
+        image: '/img/todo/todo2.webp',
+        text: 'projects.signInPage',
+        mockup: true,
+      },
+      {
+        image: '/img/todo/todo3.webp',
+        text: 'projects.signUpPage',
+        mockup: true,
+      },
+      {
+        image: '/img/todo/todo4.webp',
+        text: 'projects.todoList.emptyListPage',
+        mockup: true,
+      },
+      {
+        image: '/img/todo/todo6.webp',
+        text: 'projects.todoList.listPage',
+        mockup: true,
+      },
+      {
+        image: '/img/todo/todo5.webp',
+        text: 'projects.todoList.profilePage',
+        mockup: true,
+      },
+    ],
+    techs: ['React Native', 'Styled Components', 'Firebase'],
+  },
+  {
+    hasMockup: false,
+    title: 'Broken Out',
+    description: 'projects.brokenOut.description',
+    app: false,
+    link: 'https://broken-out.vercel.app',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/brokenOut/brokenOut1.webp',
+        text: 'projects.initialPage',
+        mockup: false,
+      },
+      {
+        image: '/img/brokenOut/brokenOut2.webp',
+        text: 'projects.aboutPage',
+        mockup: false,
+      },
+      {
+        image: '/img/brokenOut/brokenOut3.webp',
+        text: 'projects.brokenOut.gamesPage',
+        mockup: false,
+      },
+      {
+        image: '/img/brokenOut/brokenOut4.webp',
+        text: 'projects.brokenOut.selectedGamePage',
+        mockup: false,
+      },
+      {
+        image: '/img/brokenOut/brokenOut5.webp',
+        text: 'projects.signInPage',
+        mockup: false,
+      },
+      {
+        image: '/img/brokenOut/brokenOut6.webp',
+        text: 'projects.signUpPage',
+        mockup: false,
+      },
+    ],
+    techs: ['React', 'Sass', 'Materialize'],
+  },
+  {
+    hasMockup: false,
+    title: 'High Performance',
+    description: 'projects.highPerformance.description',
+    app: false,
+    link: 'https://highperformance.herokuapp.com/',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/highPerformance/high1.webp',
+        text: 'projects.initialPage',
+        mockup: false,
+      },
+      {
+        image: '/img/highPerformance/high2.webp',
+        text: 'projects.aboutPage',
+        mockup: false,
+      },
+      {
+        image: '/img/highPerformance/high5.webp',
+        text: 'projects.highPerformance.reportsPage',
+        mockup: false,
+      },
+      {
+        image: '/img/highPerformance/high4.webp',
+        text: 'projects.signUpPage',
+        mockup: false,
+      },
+      {
+        image: '/img/highPerformance/high3.webp',
+        text: 'projects.signInPage',
+        mockup: false,
+      },
+    ],
+    techs: ['HTML5 - ejs', 'Materialize', 'expressJS'],
+  },
+  {
+    hasMockup: false,
+    title: "Rafael's Trips",
+    description: 'projects.rafaels.description',
+    app: false,
+    link: '',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/trips/trip1.webp',
+        text: 'projects.initialPage',
+        mockup: false,
+      },
+      {
+        image: '/img/trips/trip2.webp',
+        text: 'projects.rafaels.salvadorPage',
+        mockup: false,
+      },
+      {
+        image: '/img/trips/trip3.webp',
+        text: 'projects.rafaels.saoPauloPage',
+        mockup: false,
+      },
+    ],
+    techs: ['Next Js', 'Styled Components', 'leaflet'],
+  },
+  {
+    hasMockup: false,
+    title: 'Estética Rhoades',
+    description: 'projects.rhoades.description',
+    app: false,
+    link: 'https://estetica-rhoades.vercel.app',
+    android: '',
+    ios: '',
+    images: [
+      {
+        image: '/img/rhoades/rhoades1.webp',
+        text: 'projects.initialPage',
+        mockup: false,
+      },
+      {
+        image: '/img/rhoades/rhoades2.webp',
+        text: 'projects.aboutPage',
+        mockup: false,
+      },
+      {
+        image: '/img/rhoades/rhoades4.webp',
+        text: 'projects.rhoades.blogPage',
+        mockup: false,
+      },
+      {
+        image: '/img/rhoades/rhoades5.webp',
+        text: 'projects.signInPage',
+        mockup: false,
+      },
+      {
+        image: '/img/rhoades/rhoades3.webp',
+        text: 'projects.rhoades.postsPage',
+        mockup: false,
+      },
+    ],
+    techs: ['Javascript', 'Firebase', 'Materialize'],
+  },
+];
---
+++ src/utils/techs.ts
@@ -0,0 +1,50 @@
+export const techs = [
+  {
+    title: 'PostgreSQL',
+    img: '/img/icons/postgresql.webp',
+  },
+  {
+    title: 'Html',
+    img: '/img/icons/html.webp',
+  },
+  {
+    title: 'Css',
+    img: '/img/icons/css.webp',
+  },
+  {
+    title: 'MySql',
+    img: '/img/icons/mysql.webp',
+  },
+  {
+    title: 'Javascript',
+    img: '/img/icons/js.webp',
+  },
+  {
+    title: 'React',
+    img: '/img/icons/react.webp',
+  },
+  {
+    title: 'React Native',
+    img: '/img/icons/reactNative.webp',
+  },
+  {
+    title: 'Typescript',
+    img: '/img/icons/ts.webp',
+  },
+  {
+    title: 'Firebase',
+    img: '/img/icons/firebase.webp',
+  },
+  {
+    title: 'Git',
+    img: '/img/icons/git.webp',
+  },
+  {
+    title: 'Jira',
+    img: '/img/icons/jira.webp',
+  },
+  {
+    title: 'Figma',
+    img: '/img/icons/figma.webp',
+  },
+];
