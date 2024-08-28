eb008b97a2f4b1e9aea03a921a31b26eac46ee3d
Rafael Santana <rafaelsantana7213@gmail.com>
2024-08-28 12:30:23 -0300

    chore: remove not used request

--- src/app/api/icon/[color]/route.ts
+++ src/app/api/icon/[color]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
-  request: NextRequest,
+  _: NextRequest,
   { params }: { params: { color: string } },
 ) {
   const color = params.color;
