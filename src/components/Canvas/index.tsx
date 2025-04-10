a43042392dcb9a01fde44bc93dd497513977bee3
Rafael Santana <rafaelsantana7213@gmail.com>
2025-04-10 20:55:32 -0300

    fix: update height classes in Canvas component for proper viewport rendering

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -16,7 +16,7 @@ export const Canvas = ({ children }: PropsWithChildren) => {
 
   if (loading)
     return (
-      <div className="!h-[100vh]">
+      <div className="!h-[100dvh]">
         <noscript>
           <Suspense fallback={null}>
             <ImageProfileNoJs />
@@ -28,7 +28,7 @@ export const Canvas = ({ children }: PropsWithChildren) => {
   return (
     <>
       <ThreeCanvas
-        className="!max-h[100vh] fixed top-0 left-0 !h-[100vh] touch-none outline-none"
+        className="fixed top-0 left-0 !h-[100dvh] touch-none outline-none"
         camera={{
           fov: 70,
           near: 0.1,
