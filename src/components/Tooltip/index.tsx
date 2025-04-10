fa0170ee78c31740a6f9ebf8d4b848ff1039559f
Rafael Santana <rafaelsantana7213@gmail.com>
2025-04-10 19:10:08 -0300

    fix: update Tooltip component to ensure proper visibility on larger screens

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -5,7 +5,7 @@ export const ToolTip = ({ children, text }: TooltipProps) => {
     <div className="group relative inline-block">
       {children}
 
-      <div className="bg-jet pointer-events-none absolute top-full left-1/2 z-10 mt-3 -translate-x-1/2 rounded px-2 py-1 text-[1.1rem] whitespace-nowrap text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+      <div className="bg-jet pointer-events-none absolute top-full left-1/2 z-10 mt-3 hidden -translate-x-1/2 rounded px-2 py-1 text-[1.1rem] whitespace-nowrap text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 md:block">
         {text}
         <div className="bg-jet absolute -top-1 left-1/2 h-2 w-2 -translate-x-1/2 rotate-45"></div>
       </div>
