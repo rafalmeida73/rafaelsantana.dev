1a1e12dc22e4663833d90e11eb3ac0c91bdf5ea2
Rafael Santana <rafael.almeida@medsystems.com.br>
2025-05-02 09:23:47 -0300

    refactor: update next.config to include bundle analyzer and optimize package imports style: clean up globals.css by removing unused styles for card components and carousel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+const withAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["icon-library"],
+  },
 };
 
-export default withNextIntl(nextConfig);
+export default withAnalyzer(withNextIntl(nextConfig));
